119a48a0d1cb891894dea47fbb9e30e7ea0f8ea1
Kateryna Rubchevska <Rubchevska.Kateryna@stud.vassboskovice.cz>
2024-05-14 09:25:48 +0200

    v1

--- EP1-Faktorial/Program.vb
+++ EP1-Faktorial/Program.vb
@@ -2,6 +2,17 @@ Imports System
 
 Module Program
   Sub Main(args As String())
-    Console.WriteLine("Hello World!")
-  End Sub
+        Dim cislo As Integer
+        Dim faktorial As Integer = 1
+
+        Console.Write(" Zadej èíslo: ")
+        cislo = Console.ReadLine()
+
+        For i = 1 To cislo
+            'faktorial = faktorial * i
+            faktorial *= i
+        Next
+        Console.WriteLine($" Faktorial èísla {cislo} je {faktorial}.")
+
+    End Sub
 End Module
